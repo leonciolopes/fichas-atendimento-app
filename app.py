1f411d0584ab4414e8e5cc868c0263a5551adae5
leonciolopes2528 <leonciolopes.2528@gmail.com>
2025-08-26 13:58:41 +0000

    Corrigido login com location e fixada versão do streamlit-authenticator

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ authenticator = stauth.Authenticate(
 )
 
 # Tela de login na sidebar
-name, auth_status, username = authenticator.login("Login", "sidebar")
+name, auth_status, username = authenticator.login(location="sidebar")
 
 # ======================
 # VERIFICAÇÃO DE LOGIN
